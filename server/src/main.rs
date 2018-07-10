4e59450225de0e3766d42b108b854d56425e179a
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-07-10 01:22:53 -0500

    create a to-be-shared statics dest

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,30 @@
 extern crate actix;
 extern crate actix_web;
+extern crate env_logger;
 
 use actix::System;
-use actix_web::{server, App, Error, HttpRequest, HttpResponse};
+use actix_web::{fs, middleware, server, App};
 
-fn index(_req: HttpRequest) -> Result<HttpResponse, Error> {
-  Ok(
-    HttpResponse::Ok()
-      .content_type("text/html")
-      .body("Hellof world"),
-  )
-}
+const URL: &str = "[::1]:8000";
 
 fn main() {
-  const URL: &str = "[::1]:8000";
+  ::std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
+
   let sys = System::new("nextrs");
 
-  let _server = server::new(|| App::new().handler("/", index))
-    .bind(URL)
+  let _server = server::new(move || {
+    App::new()
+      .middleware(middleware::Logger::default())
+      .handler(
+        "/",
+        fs::StaticFiles::new("./../static/").index_file("index.html"),
+      )
+  }).bind(URL)
     .unwrap()
     .start();
 
   println!("Started http server: {}", URL);
+
   let _ = sys.run();
 }
